0ab591c85017bd20e0dbc5286a2865af852875cc
imkhoat <khoatbui.idea@gmail.com>
2025-03-23 10:59:55 +0700

    fix: resolve issue inport and database connect

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './auth.strategy';
 import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
